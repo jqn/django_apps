08758959e213e00420b9821e0b7520d7d53841fb
jqn <jqn@Macbooks-MacBook-Pro.local>
2016-08-05 14:34:46 -0600

    Added blog templates and urls

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+# Blog app urls
+from django.conf.urls import url
+from . import views
+
+# Blog patterns
+urlpatterns = [
+    # View post_list
+    url(r'^$', views.post_list, name='post_list'),
+]
