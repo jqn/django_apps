91a6b129e6102a7eef41e04bf3d330e344a3b35c
jqn <jqn@Macbooks-MacBook-Pro.local>
2016-09-01 13:50:23 -0600

    Clean up and log update

--- error_logger/logger.py
+++ error_logger/logger.py
@@ -4,6 +4,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename='./tmp/jqnapp.log',
+                    # filemode='w',
                     )
 
 logger = logging.getLogger()
@@ -13,4 +14,11 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-logger.debug('often makes a very good meal of %s', 'visiting tourists')
+logger.debug('often makes a very good meal of %s', 'visiting tourists', exc_info=True)
+
+# Example with full stack trace
+# while True:
+#     try:
+#         main_loop()
+#     except Exception:
+#         logger.error("Fatal error in main loop", exc_info=True)
